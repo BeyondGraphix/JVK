3ac27c529fc4f0663724371a9c53b753ab1fb1e6
BeyondGraphix <afari_k@yahoo.com>
2016-02-28 09:12:50 +0000

    Added files via upload

---
+++ about.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="keywords" content="Graphic Design in Ghana, Logo design, Best logos in Ghana, Brochures, Print services in Ghana, ghana, logos, icons,">
+		<meta name="p:domain_verify" content="17d2d0509ded86e9088a4aaba2adf846"/>
+		<title>About JVK Clothing</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/css.css">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/font-awesome.css">
+		<link rel="stylesheet" href="css/animate.css">
+		
+		<!-- For IE 9 and below. ICO should be 32x32 pixels in size -->
+		<!--[if IE]><link rel="shortcut icon" href="path/to/favicon.ico"><![endif]-->
+
+		<!-- Touch Icons - iOS and Android 2.1+ 180x180 pixels in size. --> 
+
+		<link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+		<link rel="manifest" href="favicon/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+
+		<!-- Firefox, Chrome, Safari, IE 11+ and Opera. 196x196 pixels in size. 
+		<link rel="icon" href="images/riveroaks_favicon1.png">-->
+
+	</head>
+	
+	<body  id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+		<script src="js/jquery-1.9.1.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<!--The SimpleCart(js) script-->
+		<script src="js/simpleCart.js" type="text/javascript" charset="utf-8"></script>
+		
+		<div class="container-fluid ">
+			<div class="row logo" >
+
+				<div class="col-md-6 col-sm-6 col-xs-12 log animated rubberBand" style="margin:10px 0;">
+					<div id="logo"><a href="index.php"><img src="images/jvklogo.png" /></a></div>
+				</div>
+				<div class="col-md-6 col-sm-6 col-xs-12 text-center social">
+					<div class="">
+						<a href="https://www.instagram.com/jvk_clothing/" target="_blank"><i class="fa fa-instagram fa-2x pink"></i></a>
+						<a href="#"><i class="fa fa-twitter fa-2x pink"></i></a>
+						<a href="#"><i class="fa fa-google-plus fa-2x pink"></i></a>
+						<a href="#"><i class="fa fa-pinterest fa-2x pink"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>		
+		
+		
+		
+		<div class="navbar navbar-inverse ">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar-content">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					</button>
+				</div>
+				<div class="collapse navbar-collapse" id="mynavbar-content">
+					<ul class="nav navbar-nav">
+						<li class="" ><a href="index.php">Home</a></li>
+						<li class="dropdown"><a href="projects.php" class="dropdown-toggle" data-toggle="dropdown" >Shop<span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li class=""><a href="products.php">Blazers</a></li>							
+								<li class=""><a href="products.php#skirts">Skirts</a></li>
+								<li class=""><a href="products.php#tops">Tops</a></li>
+							</ul>
+						</li>
+						<li class="active"><a class="page-scroll" href="about.php">About Us</a></li>
+						<li class=""><a class="page-scroll" href="contact.php">Contact Us</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right" >
+						<li><a href="checkout.php"><span class="simpleCart_quantity"></span><span class="fa fa-cart-plus"></span> Cart</a></li>
+						<!--<li><a href="index.php"><span class="fa fa-user"></span> Account</a></li>-->
+					</ul>
+				</div>
+			</div>
+		</div>
+		
+		<div class="page-content">
+			<!-- Page Content -->
+			<div class="container">
+
+				<div class="row margin-vert-30">
+					<h1 class="black text-center">About JVK Clothing</h1><br><br>
+					
+				
+					<div class="col-md-9 col-sm-8 text-center welcomeMsg">
+						<!--<h2>Nulla in enim quis</h2>-->
+						<p class="text-justify OpenSans-Light black">JVK Clothing is a line of fun blazers,skirts and tops made from quality African fabric and inspired by cosmopolitan design that offers versatility and supreme value.
+						</p>
+
+						<br><br><br><br><br><br>
+
+					</div>
+					
+					
+					
+					<!-- End Main Text -->
+					<!-- Side Column -->
+					
+					<div class="col-md-3 col-sm-4">
+						<div class="widget wid-categories">
+							<div class="heading"><h4>CATEGORIES</h4></div>
+							<div class="content text-center">
+								<ul>
+									<li><a class="black" href="products.php#">Blazers</a></li>
+									<li><a class="black" href="products.php#skirts">Skirts</a></li>
+									<li><a class="black" href="products.php#tops">Tops</a></li>
+								</ul>
+							</div>
+						</div><br/><br><br>
+					</div>
+
+				</div>
+
+			</div>
+		</div>
+		
+		<div id="footer">
+			<div class="container">
+				<div class="row">
+					<div id="copyright" class="col-md-6 col-sm-6 col-xs-6 grey">
+						<p class="oxygen-regular">&copy; 2015 JVK Clothing</p>
+					</div>
+					<div id="footermenu" class="col-md-6 col-sm-6 col-xs-6 grey pull-right">
+						<div class="pull-right">
+							<a href="#"><i class="fa fa-facebook "></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-google-plus"></i></a>
+							<a href="#"><i class="fa fa-pinterest"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+	    <script src="js/jquery.easing.min.js"></script>	
+		
+		<script src="js/bg.js"></script>
+		
+		<script>
+		
+		</script>
+		
+	</body>
+</html>
